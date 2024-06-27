8e38f32bea23c985c387d82298cfdb6f9c172ae6
Faiz <faizkhan8225@gmail.com>
2024-06-27 13:50:04 +0530

    Add routing request and send response

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
-const http = require("http")
+const http = require("http");
+const fs = require("fs")
 
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method, req.headers);
-    // process.exit()
-})
+  const url = req.url;
+  const method = req.method
 
-server.listen(3000)
+  if (url === "/") {
+    res.write(
+      '<html><head><title>My first page</title></head><body><form action="/message" method="POST"><input type="text" name="message" /><button type="submit">Submit</button></form></body></html>'
+    );
+    return res.end()
+  }
+
+  if (url === "/message" && method === "POST") {
+    fs.writeFileSync("message.txt", "DUMMY")
+    res.statusCode = 302
+    res.setHeader("Location", "/")
+    return res.end()
+  }
+
+  res.write('<html><head><title>My first page</title></head><body><h1>Hello From Node Server.</h1></body></html>')
+});
+
+server.listen(3000);
