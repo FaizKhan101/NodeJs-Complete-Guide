3870095076672d88940f009281f62a0baf35e06c
Faiz <faizkhan8225@gmail.com>
2024-07-02 07:54:13 +0530

    Parsing incoming request (Extact form data )

--- app.js
+++ app.js
@@ -1,25 +1,35 @@
-const express = require("express")
+const express = require("express");
+const bodyParser = require("body-parser")
 
-const app = express()
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: false}))
 
 app.use("/", (req, res, next) => {
-    console.log("This always run.");
-    next()
-})
+  console.log("This always run.");
+  next();
+});
 
 app.use("/add-product", (req, res, next) => {
-    res.send("<h1>Add-Product</h1>")
-})
+  res.send(
+    '<form action="product" method="POST"><input type="text" name="title"/><button type="submit">Add Product</button></form>'
+  );
+});
+
+app.use("/product", (req, res, next) => {
+    console.log(req.body.title);
+  res.send("<h1>Product Added</h1>");
+});
 
 app.use("/", (req, res, next) => {
-    console.log("In the another middleware.");
-    res.send("<h1>Hello From ExpressJs</h1>")
-})
+  console.log("In the another middleware.");
+  res.send("<h1>Hello From ExpressJs</h1>");
+});
 
 app.listen(3000, (error) => {
-    if (error) {
-        console.log("Some error occured.");
-    }else {
-        console.log("Server start listening at post 3000.");
-    }
-})
+  if (error) {
+    console.log("Some error occured.");
+  } else {
+    console.log("Server start listening at post 3000.");
+  }
+});
