f76b1a2a5e0903e66e3d742ee3591ceb1e16894f
Faiz <faizkhan8225@gmail.com>
2024-07-02 08:10:46 +0530

    Using express router and refactor code.

--- app.js
+++ app.js
@@ -1,30 +1,15 @@
 const express = require("express");
 const bodyParser = require("body-parser")
 
+const adminRoutes = require("./routes/admin")
+const shopRoutes = require("./routes/shop")
+
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}))
 
-app.use("/", (req, res, next) => {
-  console.log("This always run.");
-  next();
-});
-
-app.use("/add-product", (req, res, next) => {
-  res.send(
-    '<form action="product" method="POST"><input type="text" name="title"/><button type="submit">Add Product</button></form>'
-  );
-});
-
-app.use("/product", (req, res, next) => {
-    console.log(req.body.title);
-  res.send("<h1>Product Added</h1>");
-});
-
-app.use("/", (req, res, next) => {
-  console.log("In the another middleware.");
-  res.send("<h1>Hello From ExpressJs</h1>");
-});
+app.use(adminRoutes)
+app.use(shopRoutes)
 
 app.listen(3000, (error) => {
   if (error) {
