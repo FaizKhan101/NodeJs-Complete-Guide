4c890401f9dc405806e4f5eb0386748804ef228a
Faiz <faizkhan8225@gmail.com>
2024-07-05 18:32:45 +0530

    Able to insert a product in products table

--- models/product.js
+++ models/product.js
@@ -9,10 +9,15 @@ module.exports = class Product {
     this.id = id;
   }
 
-  save() {}
+  save() {
+    return db.execute(
+      `INSERT INTO products (title, imageUrl, price, description) VALUES (?, ?, ?, ?)`,
+      [this.title, this.imageUrl, this.price, this.description]
+    );
+  }
 
   static fetchAll() {
-    return db.execute(`SELECT * FROM products`)
+    return db.execute(`SELECT * FROM products`);
   }
   static findById(id) {}
 
