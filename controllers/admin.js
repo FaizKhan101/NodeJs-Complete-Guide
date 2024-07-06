3540129f11df91a0d47af82ede93540d903ae0fd
Faiz <faizkhan8225@gmail.com>
2024-07-06 12:08:51 +0530

    Insert product via sequelize.

--- controllers/admin.js
+++ controllers/admin.js
@@ -14,15 +14,15 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const product = new Product(title, imageUrl, description, price);
-  product
-    .save()
-    .then((result) => {
-      res.redirect("/");
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+  Product.create({
+    title: title,
+    imageUrl: imageUrl,
+    price: price,
+    description: description,
+  }).then((result) => {
+    console.log("Product Created!");
+    res.redirect("/admin/products");
+  });
 };
 
 exports.getProducts = (req, res, next) => {
