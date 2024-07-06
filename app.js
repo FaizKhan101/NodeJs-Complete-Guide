2f0b3dbe635a3a72b28def409f23760693ce040b
Faiz <faizkhan8225@gmail.com>
2024-07-06 12:47:49 +0530

    Make association between user and product.

--- app.js
+++ app.js
@@ -6,14 +6,15 @@ const bodyParser = require("body-parser");
 const errorController = require("./controllers/error");
 const rootDir = require("./util/path");
 const sequelize = require("./util/database");
+const Product = require("./models/product");
+const User = require("./models/user");
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
 const app = express();
 
 app.set("view engine", "ejs");
 app.set("views", "views");
-
-const adminRoutes = require("./routes/admin");
-const shopRoutes = require("./routes/shop");
 
 app.use(express.static(path.join(rootDir, "public")));
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -23,7 +24,11 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
+Product.belongsTo(User, { constraints: false, onDelete: "CASCADE" });
+User.hasMany(Product);
+
 sequelize
+  //   .sync({ force: true })
   .sync()
   .then((result) => {
     // console.log(result);
--- models/user.js
+++ models/user.js
@@ -2,7 +2,7 @@ const { Sequelize } = require("sequelize")
 
 const sequelize = require("../util/database")
 
-const User = new sequelize.define("user", {
+const User = sequelize.define("user", {
     id: {
         type: Sequelize.INTEGER,
         allowNull: false,
