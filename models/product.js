c37b1894de9d3c8fc9b5f447835b78cabd88a7fa
Faiz <faizkhan8225@gmail.com>
2024-07-06 11:32:51 +0530

    Create a Product model with sequelize.

--- models/product.js
+++ models/product.js
@@ -1,27 +1,27 @@
-const db = require("../util/database");
+const { Sequelize } = require("sequelize")
 
-module.exports = class Product {
-  constructor(title, imageUrl, description, price, id) {
-    this.title = title;
-    this.imageUrl = imageUrl;
-    this.description = description;
-    this.price = +price;
-    this.id = id;
-  }
+const sequelize = require("../util/database")
 
-  save() {
-    return db.execute(
-      `INSERT INTO products (title, imageUrl, price, description) VALUES (?, ?, ?, ?)`,
-      [this.title, this.imageUrl, this.price, this.description]
-    );
+const Product = sequelize.define('product', {
+  id: {
+    type: Sequelize.STRING,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true
+  },
+  title: Sequelize.STRING,
+  imageUrl: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  price: {
+    type: Sequelize.DOUBLE,
+    allowNull: false
+  },
+  description: {
+    type: Sequelize.STRING,
+    allowNull: false
   }
+})
 
-  static fetchAll() {
-    return db.execute(`SELECT * FROM products`);
-  }
-  static findById(id) {
-    return db.execute("SELECT * FROM products WHERE id = ?", [id])
-  }
-
-  static deleteById(id) {}
-};
+module.exports = Product
