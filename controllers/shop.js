59563a381c732122e86862f385b8f32997e832d3
Faiz <faizkhan8225@gmail.com>
2024-07-07 10:23:11 +0530

    Add product in cart

--- controllers/shop.js
+++ controllers/shop.js
@@ -61,10 +61,35 @@ exports.getCart = (req, res, next) => {
 
 exports.postCart = (req, res, next) => {
   const productId = req.body.productId;
-  Product.findByPk(productId, (product) => {
-    Cart.addToCart(productId, product.price);
-  });
-  res.redirect("/cart");
+  let fetchedCart;
+  let newQuantity = 1;
+  req.user
+    .getCart()
+    .then((cart) => {
+      fetchedCart = cart;
+      return cart.getProducts({ where: { id: productId } });
+    })
+    .then((products) => {
+      let product;
+      if (products.length > 0) {
+        product = products[0];
+      }
+      if (product) {
+        let oldQuantity = product.cartItem.quantity;
+        newQuantity = ++oldQuantity;
+        return product
+      }
+      return Product.findByPk(productId);
+    })
+    .then((product) => {
+      return fetchedCart.addProduct(product, {
+        through: { quantity: newQuantity },
+      });
+    })
+    .then(() => {
+      res.redirect("/cart");
+    })
+    .catch((err) => console.log(err));
 };
 
 exports.cartDeleteProduct = (req, res, next) => {
