3e72ea6cf7bcb55042ef160086908a93939852af
Faiz <faizkhan8225@gmail.com>
2024-07-11 13:13:23 +0530

    add a method to add to cart product.

--- models/user.js
+++ models/user.js
@@ -1,20 +1,40 @@
 const { ObjectId } = require("mongodb");
-const db = require("../util/database")
+const db = require("../util/database");
+const { QueryInterface } = require("sequelize");
 
 class User {
-    constructor(name, email) {
-        this.name = name;
-        this.email = email
-    }
+  constructor(name, email, cart, id) {
+    this.name = name;
+    this.email = email;
+    this.cart = cart; // {items: []}
+    this._id = id;
+  }
 
+  save() {
+    return db.getDb().collection("users").insertOne(this);
+  }
 
-    save() {
-        return db.getDb().collection("users").insertOne(this)
-    }
+  addToCart(product) {
+    // const cartProduct = this.cart.items.findIndex(cp => {
+    //     return cp._id === product._id
+    // })
 
-    static findById(id) {
-        return db.getDb().collection("users").findOne({_id: new ObjectId(id)})
-    }
+    const updatedCart = { items: [{ ...product, quantity: 1 }] };
+    return db
+      .getDb()
+      .collection("users")
+      .updateOne(
+        { _id: new ObjectId(this._id) },
+        { $set: { cart: updatedCart } }
+      );
+  }
+
+  static findById(id) {
+    return db
+      .getDb()
+      .collection("users")
+      .findOne({ _id: new ObjectId(id) });
+  }
 }
 
 module.exports = User;
