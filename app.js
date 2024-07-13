6e29aa11a1676929996854e1e459b070ea243018
Faiz <faizkhan8225@gmail.com>
2024-07-13 13:47:16 +0530

    Add mongoose and start working with mongoose.

--- app.js
+++ app.js
@@ -2,12 +2,10 @@ const path = require("path");
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const errorController = require("./controllers/error");
 const rootDir = require("./util/path");
-
-const db = require("./util/database");
-const User = require("./models/user")
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
@@ -20,23 +18,24 @@ app.set("views", "views");
 app.use(express.static(path.join(rootDir, "public")));
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use((req, res, next) => {
-  User.findById("668d27e372dfc5de98c06903")
-    .then((user) => {
-      req.user = new User(user.name, user.email, user.cart, user._id);
-      next();
-    })
-    .catch((err) => console.log(err));
-});
+// app.use((req, res, next) => {
+//   User.findById("668d27e372dfc5de98c06903")
+//     .then((user) => {
+//       req.user = new User(user.name, user.email, user.cart, user._id);
+//       next();
+//     })
+//     .catch((err) => console.log(err));
+// });
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-db.mongoConnect()
+mongoose
+  .connect("mongodb://localhost:/27017/shop")
   .then((result) => {
-    app.listen(3000)
+    app.listen(3000, () => console.log("Server start at port: 3000"));
   })
   .catch((err) => {
     console.log(err);
