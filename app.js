da3ae177e963258d2ff0c5929aca08e6c5b72d5d
Faiz <faizkhan8225@gmail.com>
2024-07-13 14:58:57 +0530

    Add and use user model.

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const mongoose = require("mongoose");
 
 const errorController = require("./controllers/error");
 const rootDir = require("./util/path");
+const User = require("./models/user");
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
@@ -18,14 +19,14 @@ app.set("views", "views");
 app.use(express.static(path.join(rootDir, "public")));
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// app.use((req, res, next) => {
-//   User.findById("668d27e372dfc5de98c06903")
-//     .then((user) => {
-//       req.user = new User(user.name, user.email, user.cart, user._id);
-//       next();
-//     })
-//     .catch((err) => console.log(err));
-// });
+app.use((req, res, next) => {
+  User.findById("66924860b56adcff1b862376")
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((err) => console.log(err));
+});
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
@@ -35,6 +36,19 @@ app.use(errorController.get404);
 mongoose
   .connect("mongodb://localhost:27017/shop")
   .then((result) => {
+    return User.findOne();
+  })
+  .then((user) => {
+    if (!user) {
+      const user = new User({
+        name: "Faiz",
+        email: "test@test.com",
+        cart: { items: [] },
+      });
+      return user.save();
+    }
+  })
+  .then((user) => {
     app.listen(3000, () => console.log("Server start at port: 3000"));
   })
   .catch((err) => {
