201cc5d6897d5a1a820f3a2e475832562404b7b8
Faiz <faizkhan8225@gmail.com>
2024-07-13 14:01:41 +0530

    Adding a product schema through mongoose.

--- models/product.js
+++ models/product.js
@@ -1,40 +1,23 @@
-const { ObjectId } = require("mongodb");
-const db = require("../util/database");
+const mongoose = require("mongoose");
 
-class Product {
-  constructor(title, imageUrl, price, description, userId, id) {
-    this.title = title;
-    this.imageUrl = imageUrl;
-    this.price = price;
-    this.description = description;
-    this.userId = userId
-    this._id = id ? new ObjectId(id) : null;
-  }
+const Schema = mongoose.Schema;
 
-  save() {
-    if (this._id) {
-      return db
-        .getDb()
-        .collection("products")
-        .updateOne({ _id: new ObjectId(this._id) }, { $set: this });
-    } else {
-      return db.getDb().collection("products").insertOne(this);
-    }
-  }
-  static fetchAll() {
-    return db.getDb().collection("products").find().toArray();
-  }
+const productSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  imageUr: {
+    type: String,
+    required: true,
+  },
+});
 
-  static findById(id) {
-    return db
-      .getDb()
-      .collection("products")
-      .findOne({ _id: new ObjectId(id) });
-  }
-
-  static deleteById(id) {
-    return db.getDb().collection("products").deleteOne({ _id: new ObjectId(id) });
-  }
-}
-
-module.exports = Product;
