b132c8815a8c26ca2d8392ce883cb892caae1a0c
Faiz <faizkhan8225@gmail.com>
2024-07-23 14:25:10 +0530

    Validate password and check for equality.

--- routes/auth.js
+++ routes/auth.js
@@ -1,18 +1,36 @@
-const express = require('express');
-const check = require("express-validator").check
+const express = require("express");
+const { check, body } = require("express-validator");
 
-const authController = require('../controllers/auth');
+const authController = require("../controllers/auth");
 
 const router = express.Router();
 
-router.get('/login', authController.getLogin);
+router.get("/login", authController.getLogin);
 
-router.get('/signup', authController.getSignup);
+router.get("/signup", authController.getSignup);
 
-router.post('/signup', check('email').isEmail().withMessage("Please enter a valid email.") ,authController.postSignup);
+router.post(
+  "/signup",
+  [
+    check("email").isEmail().withMessage("Please enter a valid email."),
+    body(
+      "password",
+      "Please enter a password with only number and text and at least 5 character long."
+    )
+      .isLength({ min: 5 })
+      .isAlphanumeric(),
+    body("confirmPassword").custom((value, {req}) => {
+      if (value !== req.body.password) {
+        throw new Error("Password have to match.");
+      }
+      return true;
+    }),
+  ],
+  authController.postSignup
+);
 
-router.post('/login', authController.postLogin);
+router.post("/login", authController.postLogin);
 
-router.post('/logout', authController.postLogout);
+router.post("/logout", authController.postLogout);
 
 module.exports = router;
